e8b5aada9e420db3c836f90500557bea518b3d6c
Seth Schori <sethschori@users.noreply.github.com>
2018-05-07 19:13:51 -0400

    Add moz_secrets.py and twitter_secrets.py
    - Add two files for storing Twitter and Moz access tokens / IDs.
    - Will consider refactoring this in the future to store these tokens/IDs differently, but for initial version of the package, this is sufficient.

---
+++ credentials/moz_secrets.py
@@ -0,0 +1,2 @@
+ACCESS_ID = 'mozscape-add-remainder-of-access-id-here'
+SECRET_KEY = 'add-secret-key-here'
---
+++ credentials/twitter_secrets.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = 'add-consumer-key here'
+CONSUMER_SECRET = 'add-consumer-secret-here'
+ACCESS_TOKEN = 'add-access-token-here'
+ACCESS_TOKEN_SECRET = 'add-access-token-secret-here'
